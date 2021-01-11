2c9bebe979f259f5b0fad2a53b0813382ead0751
syasya-n <yousuihiromi@gmail.com>
2021-01-11 21:47:42 +0900

    既読機能のフロントエンド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,13 +10,16 @@ class PostsController < ApplicationController
   end
 
   def checked
+    #  checked.jsからsend()で送られてきた情報
     post = Post.find(params[:id])
     if post.checked 
+      # 既読なら解除するためにfalseに置き換える
       post.update(checked: false)
     else
+      # 未読なら既読にするためtrueに変更
       post.update(checked: true)
     end
-
+    # 更新したレコードを取得し直し、checked.jsへjson形式でデータを返却 
     item = Post.find(params[:id])
     render json: {post: item}
   end
